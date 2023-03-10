b1cdba9ac288dceea328c191acdd47da6be37b33
julia_topolska <topolsk92@gmail.com>
2023-03-10 14:23:06 +0200

    added new page with services

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Home/Footer/Footer';
 import Home from './components/Home/Home';
+import ServiceList from './components/Home/MainContent/ServiceList';
 import Navbar from './components/Home/Navbar/Navbar';
 import Shop from './components/Shop/Shop';
 
@@ -15,6 +16,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<Home />}></Route>
                     <Route path='/shop' element={<Shop />}></Route>
+                    <Route path='/services/:id' element={<ServiceList />}></Route>
                 </Routes>
 
                 <Footer />
--- src/components/Home/MainContent/Services.js
+++ src/components/Home/MainContent/Services.js
@@ -1,27 +1,28 @@
+import { Link } from 'react-router-dom';
 import cls from './Services.module.css';
 
 const Services = () => {
     return (
         <section>
             <div className={ cls.container }>
-                <div className={ `${cls.small} ${cls.right_marg}` }>
+                <Link to='/services/0' className={ `${cls.small} ${cls.right_marg}` }>
                     <img src={`/images/${'treatments'}.jpg`} alt='Service' />
                     <p>TREATMENTS</p>
-                </div>
-                <div className={ `${cls.big} ${cls.left_marg}` }>
+                </Link>
+                <Link to='/services/1' className={ `${cls.big} ${cls.left_marg}` }>
                     <img src={`/images/${'thermal-bath'}.jpg`} alt='Service' />
                     <p>THERMAL BATH</p>
-                </div>
+                </Link>
             </div>
             <div className={ cls.container }>
-                <div className={ `${cls.big} ${cls.right_marg}` }>
+                <Link to='/services/2' className={ `${cls.big} ${cls.right_marg}` }>
                     <img src={`/images/${'massages'}.jpg`} alt='Service' />
                     <p>MASSAGES</p>
-                </div>
-                <div className={ `${cls.small} ${cls.left_marg}` }>
+                </Link>
+                <Link to='/services/3' className={ `${cls.small} ${cls.left_marg}` }>
                     <img src={`/images/${'aromatic-oils'}.jpg`} alt='Service' />
                     <p>AROMATIC OILS</p>
-                </div>
+                </Link>
             </div>
         </section>
     );
